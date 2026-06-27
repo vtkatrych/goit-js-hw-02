de9ff26d22dbe09298aad0679a61f6d465434fd5
Vitalii <katrychvitalii23@gmail.com>
2026-06-28 00:17:52 +0300

    finished all four js

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,14 @@
-function getElementWidth(content, padding, border) {
-  const width =
-    Number.parseFloat(content) +
-    Number.parseFloat(padding) * 2 +
-    Number.parseFloat(border) * 2;
-  return width;
+function checkForSpam(message) {
+  const lowerCaseMessage = message.toLowerCase();
+  const hasSpam = lowerCaseMessage.includes("spam");
+  const hasSale = lowerCaseMessage.includes("sale");
+  return hasSpam || hasSale;
 }
 
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
